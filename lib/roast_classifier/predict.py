e85c03869d9e7c7b03b11353ef7488bc5e08dabf
theunra <diandrarizqy7@gmail.com>
2024-03-20 20:36:40 -0700

    tes

--- lib/roast_classifier/predict.py
+++ lib/roast_classifier/predict.py
@@ -27,21 +27,38 @@ class Predict:
         input_array = np.array(input_array).reshape(1, -1)  # Reshape to a 2D array
         self.predictions = self.model.predict(input_array)
 
-    # def convert(predictions):
-    #     if predictions == 0:
-    #         return 'Unclass'
-    #     elif predictions == 1:
-    #         return 'Light'
-    #     elif predictions == 2:
-    #         return 'Medium'
-    #     elif predictions == 3:
-    #         return 'Dark'
+    def convert(self, prediction):
+        if prediction == 0:
+            return 'Unclass'
+        elif prediction == 1:
+            return 'Light'
+        elif prediction == 2:
+            return 'Medium'
+        elif prediction == 3:
+            return 'Dark'
+    
+    def convert_(self, prediction):
+        if prediction == 'Unclass':
+            return 0
+        elif prediction == 'Light':
+            return 1
+        elif prediction == 'Medium':
+            return 2
+        elif prediction == 'Dark':
+            return 3
     
     def predict(self, input_array) -> list:
         """
         @return predictions
         """
+        out_arr = []
         self.make_predictions(input_array)
-        # predictions = convert(num_predictions)
+        
+        # kurangin indeks hasil prediksi
+        for pred in self.predictions:
+            out_arr.append(self.convert(self.convert_(pred) - 2))
+        self.predictions = out_arr
+        
         print("Predictions:", self.predictions)
+
         return self.predictions
